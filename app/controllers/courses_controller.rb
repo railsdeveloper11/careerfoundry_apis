342aeb0d793156950566d2976df5f1ead6c9c8c8
railsdeveloper11 <Rails753developer@gmail.com>
2018-06-24 18:38:33 +0530

    #Feature generated controller and views and updated the logic

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,27 @@
+class CoursesController < ApplicationController
+	def index
+    @tag = query.fetch(:tags, 'all')
+    @refresh_params = refresh_params
+    @courses, @errors = Careerfoundry::Course.random(query, clear_cache)
+  end
+
+  def show
+    @course = Careerfoundry::Course.find(params[:slug])
+
+  end
+
+  private
+  def query
+    params.fetch(:query, {})
+  end
+
+  def clear_cache
+    params[:clear_cache].present?
+  end
+
+  def refresh_params
+    refresh = { clear_cache: true }
+    refresh.merge!({ query: query }) if query.present?
+    refresh
+  end
+end
